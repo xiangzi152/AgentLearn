968b73238f00fe4988a2ba98250342117eee0850
tianya <tianya152111@gmail.com>
2019-10-20 02:02:15 +0800

    update submodule

--- InstrumentLearn/src/main/java/com/instrument/agent/MyInstrument.java
+++ InstrumentLearn/src/main/java/com/instrument/agent/MyInstrument.java
@@ -1,7 +1,10 @@
+
 package com.instrument.agent;
+
 
 public class MyInstrument {
     public static void main(String[] args) {
         System.out.println("This is my instrument test module...");
     }
+
 }
---
+++ InstrumentLearn/src/test/java/agent/test/Test.java
@@ -0,0 +1,10 @@
+package agent.test;
+
+public class Test {
+
+    @org.junit.Test
+    public void test() {
+
+        System.out.println("........");
+    }
+}
